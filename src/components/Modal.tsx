8064125ed450a57b73ebc7d40a83b720c8da170e
asilbekali <asilbekabdugafforov7@gmail.com>
2025-06-11 14:20:43 +0500

    finished project nav bar section

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,7 +4,7 @@ import { CloseIcon } from "../assets/icons"
 
 const Modal: FC<{ mode: boolean, setMode: Dispatch<SetStateAction<boolean>> }> = ({ mode, setMode }) => {
     return (
-        <div className={`fixed top-0 bottom-0 ${mode ? "left-0" : " left-[100%]"} right-0 bg-[#00000027] backdrop-blur-[5px]`}>
+        <div id="wrapper" onClick={(e) => (e.target as HTMLDivElement).id == "wrapper" && setMode(false)} className={`fixed top-0 bottom-0 ${mode ? "left-0" : " left-[100%]"} right-0 bg-[#00000027] backdrop-blur-[5px]`}>
             <div className={`absolute duration-300 h-[100vh] ${mode ? "right-0" : "right-[-100%]"} w-[75%] bg-[#fff] shadow-md`}>
                 <button onClick={() => setMode(false)} className={`absolute ${mode ? "" : "hidden"} right-[15px] top-[5px] text-[var(--clt-text)]`}> <CloseIcon /> </button>
             </div>
